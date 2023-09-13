578d2a212680472621a16c7034b782817042ed76
jibin <jibinvinod@gmail.com>
2023-09-13 13:13:34 +0530

    add python

---
+++ helloworld.py
@@ -0,0 +1,3 @@
+a=input("enter a word")
+string_length=(len(a))
+print(a)
---
+++ index.py
@@ -0,0 +1,32 @@
+print("hello")
+a="python"
+
+# strings
+print(a[1])
+print(a[0:3])
+print(a[:2])
+print(a[0:])
+print(a[::-1])
+print(a[-2])
+
+# to count number
+print(len(a))
+
+# to convert uppercase
+print(a.upper())
+
+# to convert lowercase
+print(a.lower())
+
+# to replace
+print(a.replace("y","z"))
+
+# wheather it startwith
+print(a.startswith("p"))
+print(a.startswith("y")) 
+
+
+# wheather it ends with 
+print(a.endswith("n"))
+print(a.endswith("y")) 
+

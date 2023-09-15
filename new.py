9a21632a707812dd4744e05d0a935979e42e7ed1
jibin <jibinvinod@gmail.com>
2023-09-15 12:47:57 +0530

    add python

---
+++ new.py
@@ -0,0 +1,37 @@
+# # set 
+s={1,2,1,"bca",6.2,2,2,1,3}
+print(s)
+
+# # to add sets 
+s.add(100)
+s.add(600)
+s.update({"banana","orange"})
+print(s)
+
+# # to remove sets 
+s.remove("bca")
+print(s)
+
+# # questions
+# fruits=({"apple","banana","cherry"})
+# print(fruits)
+# fruits.add("pineapple")
+# print(fruits)
+# fruits=({"apple","banana","cherry","mango","guva"})
+# print(fruits)
+# fruits={"apple","banana","cherry",1,2,3,4.4}
+# print(fruits)
+# fruits={"apple","banana","cherry"}
+# fruits.remove("cherry")
+# print(fruits)
+# fruits={"apple","banana","cherry"}
+# fruits.remove({"apple,banana,cherry"})
+# # print(fruits)
+
+# dictionary
+s={"name":"abc","age":23,"rollno":23}
+print(s)
+
+# to change name 
+s["name"]="python"
+print(s["name"])

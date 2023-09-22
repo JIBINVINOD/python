e9bc48ee007bd1940f4ae41338414c1dd97fb2e0
jibin <jibinvinod@gmail.com>
2023-09-22 13:18:30 +0530

    add python

--- loop.py
+++ loop.py
@@ -7,6 +7,11 @@
 #     for i in range (1,11):
         # print(i,'x',a,'=',i*a)
         # ------------
+# a=int(input("enter a number"))
+# for i in range(a+1):
+#     print(i*'*')
+# ----------------------------------------
+
 a=int(input("enter a number"))
-for i in range(a+1):
+for i in range(a,0,-1):
     print(i*'*')
